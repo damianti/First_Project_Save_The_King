fa38565c1dd98bea1110bda547f6d1724f4586d8
damianti <92877336+damianti@users.noreply.github.com>
2022-06-08 15:48:29 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+
+#include <Controller.h>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+
+
+int main ()
+{
+
+	Controller controller; 
+
+	controller.run();
+
+	
+
+	return EXIT_SUCCESS;
+}
+
+
